526c2d1cbb651e82aecc1a2fd2c7241a22811932
Dahliah Lestari <dahliahlestari@Dahliahs-MacBook-Air.local>
2026-01-12 19:20:36 +0700

    refine mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,7 @@ import { NavLink, Link } from "react-router-dom"
 export default function Navbar() {
   return (
     <header className="fixed top-0 w-full z-50 backdrop-blur border-b border-white/10">
-    <div
-      className="
-        max-w-6xl mx-auto
-        px-4 md:px-6
-        h-14 md:h-auto
-        flex justify-between items-center
-      "
-    >
-
+      <div className="max-w-5xl mx-auto px-5 py-4 flex justify-between items-center">
 
         <Link to="/" className="text-sm font-medium">
           Dahliah Lestari
